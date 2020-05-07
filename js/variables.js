e8471399a034d34048d87815f627fbaf6661edc0
Christian von Uffel <christian@cvonu.com>
2020-05-07 09:43:02 -0400

    Find "ratings" on page

--- js/variables.js
+++ js/variables.js
@@ -3,7 +3,7 @@ console.log('running Honest Marketer extension...');
 var elements = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a, span"); // TODO: support 'div'
 
 var wordTypes = ['negative', 'neutral', 'positive']
-var negativeWords = ["!", "#1 ", "amazing", "best", "easiest", "fastest", "industry leading", "instant", "most popular", "most powerful", "perfect", "superior", "the future", "world's leading"];
+var negativeWords = ["ratings","!", "#1 ", "amazing", "best", "easiest", "fastest", "industry leading", "instant", "most popular", "most powerful", "perfect", "superior", "the future", "world's leading"];
 var neutralWords = ["absolute", "better", "easy", "everything you need", "fast", "limited time", "love", "new way", "popular", "powerful", "vote"];
 var positiveWords = [" free ", " rate", " vote", "first", "patent", "refund", "reviews", "trademark"];
 
