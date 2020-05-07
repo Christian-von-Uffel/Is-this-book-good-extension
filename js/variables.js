5540fca9c8cef9a0ff72c546995cca9462539dcc
Christian von Uffel <christian@cvonu.com>
2020-05-07 09:55:45 -0400

    Remove words that aren't "ratings"

--- js/variables.js
+++ js/variables.js
@@ -2,7 +2,6 @@ console.log('running Honest Marketer extension...');
 
 var elements = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a, span"); // TODO: support 'div'
 
-var negativeWords = ["ratings","!", "#1 ", "amazing", "best", "easiest", "fastest", "industry leading", "instant", "most popular", "most powerful", "perfect", "superior", "the future", "world's leading"];
 var wordTypes = ['ratings', 'neutral', 'positive']
 var ratingsWords = ["ratings"];
 var neutralWords = ["absolute", "better", "easy", "everything you need", "fast", "limited time", "love", "new way", "popular", "powerful", "vote"];
