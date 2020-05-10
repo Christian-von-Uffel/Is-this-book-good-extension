a3920fff92807195f6f9f024e6936167c1f210eb
Christian von Uffel <christian@cvonu.com>
2020-05-09 20:43:28 -0400

    initialize function and make oneStar variable numeric

--- js/get-book-data.js
+++ js/get-book-data.js
@@ -1,3 +1,5 @@
+function getBookData() {
+
 // Get book's average review score
 let avgReviewScore = parseInt(document.querySelector("#a-popover-content-9 > div > div > div > div.a-icon-row.a-spacing-small.a-padding-none > span").innerText.replace(" out of 5",""))
 
@@ -8,4 +10,6 @@ let reviewCount = parseInt(document.querySelector("#acrCustomerReviewText").inne
 
 
 // Get book's % of 1-star reviews
-let oneStarPercentage = document.querySelector("#histogramTable > tbody > tr:nth-child(5) > td.aok-nowrap > span.a-size-base > a").title.replace('1 stars represent ','').replace('% of rating','')
+let oneStarPercentage = parseInt(document.querySelector("#histogramTable > tbody > tr:nth-child(5) > td.aok-nowrap > span.a-size-base > a").title.replace('1 stars represent ','').replace('% of rating',''))
+
+}
